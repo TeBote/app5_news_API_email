c789322524b9695fa09eec7810621a7b216f0bfe
TeBote22 <moisa.paul@gmail.com>
2025-06-10 18:11:04 +0300

    Day 27: Send email with news title and description Student project done

--- send_article_to_email.py
+++ send_article_to_email.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "c410ab4ea8b04e5c9934fd18399a6ecb"
 url = ("https://newsapi.org/v2/everything?q=tesla&"\
@@ -11,7 +12,13 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-#Access the article titles and description
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None and article["description"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + "\n\n"
+
+body = body.encode("utf-8")
+send_email(body)
+
+
